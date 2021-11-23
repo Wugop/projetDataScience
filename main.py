97efba39a7fe0e9047cf549a43aed3f39287cbb1
wmaximilien <wagret.maximilien@gmail.com>
2021-11-23 17:39:35 +0100

    func histo

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ import seaborn as sns
 data = pd.read_csv('dataset.csv')
 
 st.pyplot((data.head(2000)['country'].hist(bins=100)).figure)
+
+
+def histogrammeVarDiscrete(data,var):
+    return data[var].hist(bins=50).figure
